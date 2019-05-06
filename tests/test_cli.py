a9ba30c324be2fd549744caa887cf7ad270b369f
Tim Birkett <sharkfinhoop@gmail.com>
2019-05-06 22:14:02 +0100

    Add a simple test

---
+++ tests/test_cli.py
@@ -0,0 +1,13 @@
+import pytest
+from click.testing import CliRunner
+from grafannotate import cli
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+def test_cli(runner, caplog):
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
+    assert 'NewConnectionError' in caplog.text
