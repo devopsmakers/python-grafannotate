85404baf92cc606b3a50fe3b297e2aa167cfeea7
Tim Birkett <sharkfinhoop@gmail.com>
2019-05-07 10:32:13 +0100

    Accept times in secs, update help messages

--- grafannotate/cli.py
+++ grafannotate/cli.py
@@ -14,14 +14,14 @@ except ImportError:
 @click.command()
 @click.option('-u', '--uri', 'annotate_uri',
               default='http://localhost:3000/api/annotations',
-              help='uri to send annotation to')
-@click.option('-T', '--title', 'title', default='event', help='event title')
-@click.option('-t', '--tag', 'tags', multiple=True, help='event tags')
-@click.option('-d', '--description', 'description', help='event description')
+              help='URI to send annotation to. Default: "http://localhost:3000/api/annotations".')
+@click.option('-T', '--title', 'title', default='event', help='Event title. Default: "event".')
+@click.option('-t', '--tag', 'tags', multiple=True, help='Event tags (can be used multiple times).')
+@click.option('-d', '--description', 'description', help='Event description body. Optional.')
 @click.option('-s', '--start', 'start_time',
-              default=int(round(time.time() * 1000)),
-              help='event start timestamp (unix secs)')
-@click.option('-e', '--end', 'end_time', default=0, help='event end timestamp (unix secs)')
+              default=int(time.time()),
+              help='Start timestamp (unix secs). Default: current timestamp.')
+@click.option('-e', '--end', 'end_time', default=0, help='End timestamp (unix secs). Optional.')
 def main(annotate_uri, title, tags, description, start_time, end_time):
     """ Send Grafana annotations """
 
@@ -42,12 +42,12 @@ def main(annotate_uri, title, tags, description, start_time, end_time):
         if 'http' in url_parts.scheme:
             event_data['text'] = '<b>%s</b>\n\n%s' % (title, description)
             event_data['tags'] = tags
-            event_data['time'] = start_time
+            event_data['time'] = int(round(start_time * 1000))
             if end_time > 0:
                 if end_time < start_time:
                     raise Exception('Event end time cannot be before start time')
                 event_data['isRegion'] = True
-                event_data['timeEnd'] = end_time
+                event_data['timeEnd'] = int(round(end_time * 1000))
 
             logging.debug(event_data)
             send_web_annotation(url_parts, event_data)
