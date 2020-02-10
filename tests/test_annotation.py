bd67ae79bf8054e355c4bb7978d0dcce0529572b
Tim Birkett <tim.birkett@sainsburys.co.uk>
2020-02-10 09:44:08 +0000

    Add annotation with API key test

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -81,6 +81,26 @@ def test_annotation_send_to_web():
             'message': 'Annotation added'
         }
 
+def test_annotation_send_to_web_with_api_key():
+    url = "http://localhost:3000/api/annotations"
+    with requests_mock.Mocker() as m:
+        m.register_uri(
+            requests_mock.POST,
+            url,
+            status_code=200,
+            json={'message': 'Annotation added'}
+        )
+        test_annotation = Annotation('event', ['test'], 'testing', 1559332960, 1559332970)
+        assert test_annotation.send(url, 'aTestKey') == {
+            'event_data': {
+                'isRegion': True,
+                'tags': ['test'],
+                'text': '<b>event</b>\n\ntesting',
+                'time': 1559332960000,
+                'timeEnd': 1559332970000
+            },
+            'message': 'Annotation added'
+        }
 
 def test_annotation_error_sending_to_web():
     url = "http://localhost:3000/api/annotations"
